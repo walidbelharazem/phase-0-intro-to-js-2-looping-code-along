a599902abb1e8df8a8236f347ea17b19d011e6bd
walidbelharazem <walidbelharazem@gmail.com>
2023-08-03 13:23:52 +0200

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Code your solutions in this file
+names=["Charlie", "Samip", "Ali"];
+function writeCards(names, event) {
+    return names.map(name => `Thank you, ${name}, for the wonderful ${event} gift!`);
+  }
+  function countDown(number) {
+    for (let i = number; i >= 0; i--) {
+      console.log(i);
+    }
+  }
+  
+  module.exports = { writeCards, countDown };  
